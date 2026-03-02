afb65ad25b725ef820d832382372607da245645b
MC - Marcos <admin@mabelcode.com.br>
2026-03-01 23:42:47 -0300

    Fix BORAFUT-8

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@/lib/dom-patch'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
